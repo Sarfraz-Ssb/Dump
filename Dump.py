728ce2347761a6d6b8d702147785e79ac441e596
Muhammad Sarfraz <99527217+Sarfraz-Ssb@users.noreply.github.com>
2023-01-28 21:16:05 +0500

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -1,8 +1,6 @@
 import os, platform
 os.system('xdg-open https://facebook.com/groups/3017062245271082/')
 os.system('git pull')
-import requests
-os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     import Dump
